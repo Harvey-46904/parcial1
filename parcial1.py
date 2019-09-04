b02608d34acec22153bc7b99ad770aa0366aaf48
sebasarturo37 <sebasarturo37@hotmail.com>
2019-09-04 09:43:02 -0500

    menu

---
+++ parcial1.py
@@ -0,0 +1,8 @@
+
+
+print(".: CARRERA NUMERICA :.")
+print ("Elegir un nivel...")
+print ("[1]Nivel Básico (Tablero de20 posiciones)")
+print ("[2]Nivel Intermedio (Tablero de 30 posiciones)")
+print ("[3]Nivel Avanzado (Tablero de 50 posiciones)")
+print ("[5]Salir")
