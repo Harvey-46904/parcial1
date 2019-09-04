4a4cf0b6e5660342786b07aef0dc4debf39d44fd
sebasarturo37 <sebasarturo37@hotmail.com>
2019-09-04 09:53:07 -0500

    v3

--- parcial1.py
+++ parcial1.py
@@ -6,3 +6,10 @@ print ("[1]Nivel Básico (Tablero de20 posiciones)")
 print ("[2]Nivel Intermedio (Tablero de 30 posiciones)")
 print ("[3]Nivel Avanzado (Tablero de 50 posiciones)")
 print ("[5]Salir")
+
+N=[]
+
+for i in range(0,x):
+    random_num = random.randrange(0,6)
+    N.append(random_num)
+print("Los numeros generados son: ", N)
