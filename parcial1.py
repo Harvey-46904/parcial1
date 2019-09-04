7ad20eaca2c01568f7eefef0eb80a4b80af3bd70
Harvey-46904 <harveympv@hotmail.com>
2019-09-04 16:09:28 -0500

    v final

--- parcial1.py
+++ parcial1.py
@@ -1,60 +1,66 @@
 import random
 posicion=0
-jugador=[]
-j1=[]
+juego=[]
+nivel=[]
+personajes=[]
+fin=True
+def niveles(nivel):
+    if nivel==1:
+        return 20
+    if nivel==2:
+        return 30
+    if nivel==3:
+        return 50
+    
 def dados():
     N=[]
     for i in range(0,2):
         random_num = random.randrange(1,6)
         N.append(random_num)
     print("Dados: ", N)
-    return N
+    suma=N[0]+N[1]
+    print("Avanza: ",suma)
+    return suma
 
-def tablerobasico():
-    posicion=20
-    return posicion
-def tablerointermedio():
-    posicion=30
-    return posicion
-def tableroavanzado():
-    posicion=50
-    return posicion
-
-def jugadores(a):
-    if a>=2 and a<=4:
-        print ("jugadores validos")
-        for i in range(1,a+1):
-            jugador.append(i)
-        print (jugador)
-    else:
-        print ("minimo 2 jugadores , maximo 4 jugadores")
-
-def menu(a):
-    if a==1:
-        print("tablero de :",tablerobasico())
-        j1.append(1)
-        j1.append(tablerobasico())
-        print(j1)
-    elif a==2:
-        print("tablero de :",tablerointermedio())
-    elif a==3:
-        print("tablero de :",tableroavanzado())
-
-
-
+def Tablero_jugador(jugadores,nivel):
+    for i in range (1,jugadores+1):
+        personajes.append(i)
+    for d in range (0,len(personajes)):
+        juego.append([personajes[d],niveles(nivel)])
+    
+    
 
 
 print(".: CARRERA NUMERICA :.")
+print("")
 print ("Elegir un nivel...")
 print ("[1]Nivel Básico (Tablero de20 posiciones)")
 print ("[2]Nivel Intermedio (Tablero de 30 posiciones)")
 print ("[3]Nivel Avanzado (Tablero de 50 posiciones)")
 print ("[5]Salir")
-
+print("")
 x=int(input("Digite cantidad de jugadores: "))
-jugadores(x)
-y=int(input("seleccion opcion: "))
-menu(y)
+
+y=int(input("Selección Nivel: "))
+Tablero_jugador(x,y)
+while fin==True:
+    for j in range (0,len(juego)):
+        print("")
+        print("TURNO JUGADOR: ",j+1)
+        avanza=dados()
+        juego[j][1]=juego[j][1]-avanza
+        
+        if juego[j][1]<=0:
+            print ("GANA JUGADOR: ",j+1)
+            fin=False
+        if juego[j][1]>=0:
+            print("posicion actual: ",juego)
+        print ("")
+        lef=input("Presione enter para continuar: ")
+        
+    
+
+print(juego)
 
 
 
