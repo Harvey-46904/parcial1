39c6475d89bdebf3c85d40491ab0f69e2ae0d4da
Harvey-46904 <harveympv@hotmail.com>
2019-09-04 09:54:53 -0500

    v4

---
+++ parcial1.py
@@ -0,0 +1,12 @@
+def jugadores(a):
+    if a>=2 and a<=4:
+        print ("jugadores validos")
+    else:
+        print ("minimo 2 jugadores , maximo 4 jugadores")
+
+
+
+
+x=int(input("Digite cantidad de jugadores: "))
+jugadores(x)
+
